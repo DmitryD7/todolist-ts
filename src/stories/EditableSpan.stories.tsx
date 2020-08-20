7258b59322c9e0a5886057803e86951c818969b8
DmitryDemid <dimon.demid15@gmail.com>
2020-08-20 15:33:07 -0400

    Creating EditableSpan storybook

---
+++ src/stories/EditableSpan.stories.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Meta} from "@storybook/react";
+import {action} from "@storybook/addon-actions";
+import {EditableSpan} from "../EditableSpan";
+
+export default {
+    title: 'Example/EditableSpan',
+    component: EditableSpan,
+} as Meta;
+
+export const EditableSpanBasic = () => {
+    return <EditableSpan title={'Hello'} setNewTitle={action('Title was chnged')}/>
+}
