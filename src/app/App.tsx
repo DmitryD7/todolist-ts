ca2682d4b2b386612c97e4286101e95a1185563d
DmitryDemid <dimon.demid15@gmail.com>
2020-09-09 18:41:03 -0400

    using new features to show error alert and status loader

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
-import {AppBar, Button, Container, IconButton, Toolbar, Typography} from "@material-ui/core";
+import {AppBar, Button, Container, IconButton, LinearProgress, Toolbar, Typography} from "@material-ui/core";
 import MenuIcon from '@material-ui/icons/Menu';
 import {TodolistsList} from "../features/TodolistsList/TodolistsList";
+import {ErrorSnackbar} from "../components/ErrorSnackBar/ErrorSnackBar";
 
 export const App = React.memo(() => {
     return <div className="App">
         <AppBar position="static">
+            <ErrorSnackbar/>
             <Toolbar>
                 <IconButton edge="start" color="inherit" aria-label="menu">
                     <MenuIcon/>
@@ -16,6 +18,7 @@ export const App = React.memo(() => {
                 </Typography>
                 <Button color="inherit">Login</Button>
             </Toolbar>
+            <LinearProgress />
         </AppBar>
         <Container fixed>
             <TodolistsList/>
