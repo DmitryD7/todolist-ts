77a224ecbf5be6c86e2a50d7c15af91e0c7e21cf
DmitryDemid <dimon.demid15@gmail.com>
2020-09-10 13:01:20 -0400

    creating new reducer to log in and log out

---
+++ src/features/Login/auth-reducer.ts
@@ -0,0 +1,45 @@
+import {Dispatch} from 'redux'
+import {SetAppErrorActionType, setAppStatusAC, SetAppStatusActionType} from "../../app/appReducer/app-reducer";
+import {authAPI} from "../../api/todolist-api";
+import {handleServerAppError, handleServerNetworkError} from "../../utils/errorUtils";
+
+
+const initialState: InitialStateType = {
+    isLoggedIn: false
+}
+
+export const authReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
+    switch (action.type) {
+        case 'login/SET-IS-LOGGED-IN':
+            return {...state, isLoggedIn: action.value}
+        default:
+            return state
+    }
+}
+// actions
+export const setIsLoggedInAC = (value: boolean) =>
+    ({type: 'login/SET-IS-LOGGED-IN', value} as const)
+
+// thunks
+
+export const logoutTC = () => (dispatch: Dispatch<ActionsType>) => {
+    dispatch(setAppStatusAC('loading'))
+    authAPI.logout()
+        .then(res => {
+            if (res.data.resultCode === 0) {
+                dispatch(setIsLoggedInAC(false))
+                dispatch(setAppStatusAC('succeeded'))
+            } else {
+                handleServerAppError(res.data, dispatch)
+            }
+        })
+        .catch(error => {
+            handleServerNetworkError(error, dispatch)
+        })
+}
+
+// types
+type ActionsType = ReturnType<typeof setIsLoggedInAC> | SetAppStatusActionType | SetAppErrorActionType
+type InitialStateType = {
+    isLoggedIn: boolean
+}
