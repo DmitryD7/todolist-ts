c7627f8cfed746513ddb377dc15f8b8d644d8177
DmitryDemid <dimon.demid15@gmail.com>
2020-09-10 13:06:00 -0400

    initializing application, using new feature logout and checking is logged in

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,19 +1,51 @@
-import React from 'react';
+import React, {useCallback, useEffect} from 'react';
 import './App.css';
-import {AppBar, Button, Container, IconButton, LinearProgress, Toolbar, Typography} from "@material-ui/core";
+import {
+    AppBar,
+    Button,
+    CircularProgress,
+    Container,
+    IconButton,
+    LinearProgress,
+    Toolbar,
+    Typography
+} from "@material-ui/core";
 import MenuIcon from '@material-ui/icons/Menu';
 import {TodolistsList} from "../features/TodolistsList/TodolistsList";
 import {ErrorSnackbar} from "../components/ErrorSnackBar/ErrorSnackBar";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {AppRootStateType} from "./store";
-import {RequestStatusType} from "./appReducer/app-reducer";
+import {Route} from 'react-router-dom';
+import {Login} from "../features/Login/Login";
+import {initializeAppTC, RequestStatusType} from "./appReducer/app-reducer";
+import {logoutTC} from "../features/Login/auth-reducer";
 
 type AppPropsType = {
     demo?: boolean
 }
 
 export const App = React.memo(({demo = false, ...props}: AppPropsType) => {
-   const status = useSelector<AppRootStateType, RequestStatusType>(state => state.app.status)
+    const status = useSelector<AppRootStateType, RequestStatusType>(state => state.app.status)
+    const isInitialized = useSelector<AppRootStateType>(state => state.app.isInitialized)
+    const isLoggedIn = useSelector<AppRootStateType, boolean>(state => state.auth.isLoggedIn)
+
+    useEffect(() => {
+        dispatch(initializeAppTC())
+    }, [])
+
+    const dispatch = useDispatch()
+
+    const logout = useCallback(() => {
+        dispatch(logoutTC())
+    }, [])
+
+    if (!isInitialized) {
+        return <div
+            style={{position: 'fixed', top: '30%', textAlign: 'center', width: '100%'}}>
+            <CircularProgress/>
+        </div>
+    }
+
     return <div className="App">
         <AppBar position="static">
             <ErrorSnackbar/>
@@ -24,12 +56,13 @@ export const App = React.memo(({demo = false, ...props}: AppPropsType) => {
                 <Typography variant="h6">
                     ToDo-Lists
                 </Typography>
-                <Button color="inherit">Login</Button>
+                {isLoggedIn && <Button color="inherit" onClick={logout}>Log out</Button>}
             </Toolbar>
             {status === 'loading' && <LinearProgress/>}
         </AppBar>
         <Container fixed>
-            <TodolistsList demo={demo}/>
+            <Route exact path={'/'} render={() => <TodolistsList demo={demo}/>}/>
+            <Route path={'/login'} render={() => <Login/>}/>
         </Container>
     </div>
 })
