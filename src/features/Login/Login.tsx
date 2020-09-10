57f326f76dd38644384c265fd3df4c62cb2e5b92
DmitryDemid <dimon.demid15@gmail.com>
2020-09-10 13:00:57 -0400

    creating new component to log in

---
+++ src/features/Login/Login.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import {Checkbox, FormControl, FormControlLabel, FormGroup, FormLabel, TextField, Button, Grid} from '@material-ui/core'
+import {Redirect} from "react-router-dom";
+import {useSelector} from "react-redux";
+import {AppRootStateType} from "../../app/store";
+
+export const Login = () => {
+    const isLoggedIn = useSelector<AppRootStateType, boolean>(state => state.auth.isLoggedIn)
+    if (isLoggedIn){
+        return <Redirect to={'/'}/>
+    }
+    return <Grid container justify="center">
+        <Grid item xs={4}>
+            <FormControl>
+                <FormLabel>
+                    <p>To log in get registered
+                        <a href={'https://social-network.samuraijs.com/'}
+                           target={'_blank'}>here
+                        </a>
+                    </p>
+                    <p>or use common test account credentials:</p>
+                    <p>Email: free@samuraijs.com</p>
+                    <p>Password: free</p>
+                </FormLabel>
+                <FormGroup>
+                    <TextField
+                        label="Email"
+                        margin="normal"
+                    />
+                    <TextField
+                        type="password"
+                        label="Password"
+                        margin="normal"
+                    />
+                    <FormControlLabel
+                        label={'Remember me'}
+                        control={<Checkbox/>}
+                    />
+                    <Button type={'submit'} variant={'contained'} color={'primary'}>Login</Button>
+                </FormGroup>
+            </FormControl>
+        </Grid>
+    </Grid>
+}
