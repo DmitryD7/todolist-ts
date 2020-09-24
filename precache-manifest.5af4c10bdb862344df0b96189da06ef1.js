568f03d5596227d9c72756c57ae43767c64a650a
DmitryDemid <dimon.demid15@gmail.com>
2020-09-23 23:26:11 -0400

    Updates

---
+++ precache-manifest.5af4c10bdb862344df0b96189da06ef1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dc22e4631d5da169356ee64290e4e86",
+    "url": "/todolist-ts/index.html"
+  },
+  {
+    "revision": "06097bd7c5e14c3e98c6",
+    "url": "/todolist-ts/static/css/main.cf3f544e.chunk.css"
+  },
+  {
+    "revision": "9f47d2a0698352c194d8",
+    "url": "/todolist-ts/static/js/2.384b0f7c.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/todolist-ts/static/js/2.384b0f7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06097bd7c5e14c3e98c6",
+    "url": "/todolist-ts/static/js/main.7418a514.chunk.js"
+  },
+  {
+    "revision": "b7c7c0b009241d8831c6",
+    "url": "/todolist-ts/static/js/runtime-main.8e0caec2.js"
+  }
+]);
